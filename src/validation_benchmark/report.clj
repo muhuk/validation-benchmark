c08f8c88af0daefcd29c548a5ba9aa63ca06ee14
Atamert Ölçgen <muhuk@muhuk.com>
2016-05-12 22:00:42 +0300

    Moved page sections into their own functions.

--- src/validation_benchmark/report.clj
+++ src/validation_benchmark/report.clj
@@ -37,12 +37,43 @@
     (make-chart summary chart-path)))
 
 
+(defn- about []
+  [:div
+   [:h2 {:id "about"} "About"]
+   [:div "Benchmark for Clojure validation libraries."]
+   [:div
+    [:a {:href "https://github.com/muhuk/validation-benchmark"}
+     "Source code"]]])
+
+
 (defn- fork-me []
   [:a {:href "https://github.com/muhuk/validation-benchmark"}
    [:img {:style "position: absolute; top: 0; right: 0; border: 0;"
           :src "https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67"
           :alt "Fork me on GitHub"
           :data-canonical-src "https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"}]])
+
+
+(defn- performance-graph []
+  [:div
+   [:h2 {:id "performance-graph"} "Performance Graph"]
+   ;; TODO: Don't hardcode filename.
+   [:img {:src "chart.png"}]])
+
+
+(defn- relative-performance-table [summary]
+  [:div
+   [:h2 {:id "relative-performance-table"} "Relative performance table"]
+   (->> (for [[t l m] summary]
+          [:tr
+           [:td (pr-str t)]
+           [:td (pr-str l)]
+           [:td (format "%10.3f" m)]])
+        (into [:table {:class "table"}
+               [:tr
+                [:th "Test name"]
+                [:th "Library"]
+                [:th "Mean (ns)"]]]))])
 
 
 (defn- render-html [summary]
@@ -74,30 +105,14 @@
                                      (local-time/local-now))]]]]]
            [:div {:class "row"}
             [:div {:class "col-md-10"}
-             [:h2 {:id "about"} "About"]
-             [:div "Benchmark for Clojure validation libraries."]
-             [:div
-              [:a {:href "https://github.com/muhuk/validation-benchmark"}
-               "Source code"]]
-             [:h2 {:id "relative-performance-table"} "Relative performance table"]
-             (->> (for [[t l m] summary]
-                    [:tr
-                     [:td (pr-str t)]
-                     [:td (pr-str l)]
-                     [:td (format "%10.3f" m)]])
-                  (into [:table {:class "table"}
-                         [:tr
-                          [:th "Test name"]
-                          [:th "Library"]
-                          [:th "Mean (ns)"]]]))
-             [:h2 {:id "performance-graph"} "Performance Graph"]
-             ;; TODO: Don't hardcode filename.
-             [:img {:src "chart.png"}]]
+             (about)
+             (relative-performance-table summary)
+             (performance-graph)]
             [:div {:id "navbar"
                    :class "col-md-2"}
              [:ul {:class "nav nav-pills nav-stacked"
                    :data-spy "affix"}
-              [:li [:a {:href "#top"} [:span {:class "glyphicon glyphicon-triangle-right"}] " Top"]]
+              [:li [:a {:href "#top"} [:span {:class "glyphicon glyphicon-triangle-top"}] " Top"]]
               [:li [:a {:href "#about"} [:span {:class "glyphicon glyphicon-triangle-right"}] " About"]]
               [:li [:a {:href "#relative-performance-table"} [:span {:class "glyphicon glyphicon-triangle-right"}] " Relative performance table"]]
               [:li [:a {:href "#performance-graph"} [:span {:class "glyphicon glyphicon-triangle-right"}] " Performance Graph"]]]]]]]))
