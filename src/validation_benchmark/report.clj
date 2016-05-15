3fb431d362061cb17515b2238b4b32874c69aa7f
Atamert Ölçgen <muhuk@muhuk.com>
2016-05-15 12:13:04 +0300

    Added goole analytics tracking code.

--- src/validation_benchmark/report.clj
+++ src/validation_benchmark/report.clj
@@ -12,6 +12,19 @@
          render-html)
 
 
+(def ga-js
+  "<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-390796-16', 'auto');
+  ga('send', 'pageview');
+
+</script>")
+
+
 (defn create-report [groups results report-path]
   (let [current-directory (System/getProperty "user.dir")
         html-path (str report-path "/index.html")
@@ -142,4 +155,5 @@
               [:li [:a {:href "#top"} [:span {:class "glyphicon glyphicon-triangle-top"}] " Top"]]
               [:li [:a {:href "#about"} [:span {:class "glyphicon glyphicon-triangle-right"}] " About"]]
               [:li [:a {:href "#relative-performance-table"} [:span {:class "glyphicon glyphicon-triangle-right"}] " Relative performance table"]]
-              [:li [:a {:href "#performance-graph"} [:span {:class "glyphicon glyphicon-triangle-right"}] " Performance Graph"]]]]]]]))
+              [:li [:a {:href "#performance-graph"} [:span {:class "glyphicon glyphicon-triangle-right"}] " Performance Graph"]]]]]
+           ga-js]]))
