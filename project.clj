1f3b9c5034e5c654414b0d24ac5a60de9e3d4d30
Atamert Ölçgen <muhuk@muhuk.com>
2016-06-16 00:30:03 +0300

    Upgraded clj-time, criterium, tools.cli & pretty.

--- project.clj
+++ project.clj
@@ -3,19 +3,19 @@
   :url "https://github.com/muhuk/validation-benchmark"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-time "0.11.0"]
+  :dependencies [[clj-time "0.12.0"]
                  [com.roomkey/annotate "1.0.1"]
+                 [com.taoensso/truss "1.3.3"]
                  [com.velisco/herbert "0.7.0-alpha2"]
-                 [criterium "0.4.3"]
+                 [criterium "0.4.4"]
                  [hiccup "1.0.5"]
                  [incanter/incanter-core "1.5.6"]
                  [incanter/incanter-charts "1.5.6"]
-                 [io.aviso/pretty "0.1.23"]
+                 [io.aviso/pretty "0.1.26"]
                  [org.clojure/clojure "1.9.0-alpha5"]
-                 [org.clojure/tools.cli "0.3.3"]
-                 [prismatic/schema "1.0.4"]
-                 [com.taoensso/truss "1.3.3"]]
-  :plugins [[io.aviso/pretty "0.1.23"]]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [prismatic/schema "1.0.4"]]
+  :plugins [[io.aviso/pretty "0.1.26"]]
   :main ^:skip-aot validation-benchmark.core
   :jvm-opts ^:replace ["-server" "-XX:+AggressiveOpts" "-Djava.awt.headless=true"]
   :target-path "target/%s"
